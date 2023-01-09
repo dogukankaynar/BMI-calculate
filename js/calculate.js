05e88370407b7491d8d90f3140621d28efd45747
dogukankaynar <dogukankaynar98@gmail.com>
2023-01-09 23:00:58 +0300

    script dosyaları moduler hale geldi

---
+++ js/calculate.js
@@ -0,0 +1,7 @@
+function calculateNow(){
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
+    let ıbw = weight/height;
+    console.log(ıbw);
+}
+export {calculateNow}
