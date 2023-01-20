e23fcb88a6a66eca211295f525aad66107e9eeba
dogukankaynar <dogukankaynar98@gmail.com>
2023-01-20 14:59:32 +0300

    vücut kitle indexsi hesaplandı

--- js/calculate.js
+++ js/calculate.js
@@ -1,8 +1,10 @@
+import {calculateReruslt} from "./result.js";
+
 const calculateNow = () => {
     let k = document.getElementById("weight").value;
     let b = document.getElementById("height").value/100;
     let ıbw = Math.round(k/ (b*b));
-    console.log(ıbw);
+    calculateReruslt(ıbw)
 };
 
 export {calculateNow}
--- js/result.js
+++ js/result.js
@@ -1,5 +1,11 @@
-const calculateReruslt =()=>{
-console.log("sa");
+const calculateReruslt =(ıbw)=>{
+    console.log(ıbw);
+    if(ıbw<=18){console.log("zayıfsın");}
+    else if(ıbw<24){console.log("ideal");}
+    else if(ıbw<29){console.log("kilolu");}
+    else if(ıbw<34){console.log("1. derece obezite");}
+    else if(ıbw<39){console.log("2. derece obezite");}
+    else if(ıbw>39){console.log("3. derece obezite");}
 };
 
 export {calculateReruslt}
